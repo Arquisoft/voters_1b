05fffb4f2f30096cb6c8908f7700e389c2d797f9
Adrian Suarez <adrivillu@gmail.com>
2016-02-15 18:07:32 +0100

    Cambio de contraseña

--- src/main/java/hello/MainController.java
+++ src/main/java/hello/MainController.java
@@ -4,8 +4,14 @@ package hello;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 
 @Controller
 public class MainController {
@@ -20,10 +26,32 @@ public class MainController {
     }
     
     @RequestMapping(value="/", method=RequestMethod.POST)
-    public String greetingSubmit(@ModelAttribute UserInfo greeting, Model model) {
+    public String greetingSubmit(@ModelAttribute UserInfo greeting, Model model, HttpSession sesion) {
   
     	model.addAttribute("greeting", greeting);
+    	sesion.setAttribute("login", greeting.getLogin());
         return "result";
+    }
+    
+    @RequestMapping(value="/cambiar-clave", method=RequestMethod.GET)
+    public String cambiarClave(Model model) {
+    	model.addAttribute("cambiarClave", new CambiarClave());
+    	return "cambiar-clave";
+    }
+    
+    @RequestMapping(value="/cambiar-clave", method=RequestMethod.POST)
+    public String cambiarClaveSubmit(@Valid @ModelAttribute CambiarClave cambiarClave, BindingResult br, HttpSession sesion) {
+    	
+    	if (br.hasErrors()) {
+    		return "cambiar-clave";
+    	}
+    	else {
+    		
+    		String login = (String)sesion.getAttribute("login");
+    		br.addError(new FieldError("cambiarClave", "claveAnterior", "Clave anterior mal"));
+    		System.out.println(login + " " + cambiarClave.getClaveAnterior() + " " + cambiarClave.getClaveNueva() + " " + cambiarClave.getConfirmacionClave());
+	    	return "redirect:/";
+    	}
     }
 
 }
