50ed5189d3888f5e740b76cf90de598bfda5e2b9
AninaGlez <aninaglezg@gmail.com>
2016-02-22 19:15:14 +0100

    Interfaz GetVoterInfo implementada  y llamada desde la peticion POST

--- src/main/java/hello/GetVoterInfo.java
+++ src/main/java/hello/GetVoterInfo.java
@@ -2,4 +2,7 @@ package hello;
 
 public interface GetVoterInfo {
 
+	public UserInfo getInfo(String email,String pass);
+	
+	
 }
--- src/main/java/hello/MainController.java
+++ src/main/java/hello/MainController.java
@@ -23,6 +23,8 @@ public class MainController {
 	@Autowired
     DBManager repository;
 	
+	GetVoterInfo infoPort;
+	
  
     @RequestMapping(value="/", method=RequestMethod.GET)
     public String greetingForm(Model model) {
@@ -36,29 +38,31 @@ public class MainController {
     public String greetingSubmit(@ModelAttribute UserInfo greeting, Model model, HttpSession sesion) {
   
     	model.addAttribute("userinfo", greeting);
-    	
+    	infoPort= new VoterAccess(repository);
     	try{
     		//busco el usuario por el login
-    		List<UserInfo> users=repository.findByLogin(greeting.getLogin());
+    		//List<UserInfo> users=repository.findByLogin(greeting.getLogin());
         	
-        	if (users!=null){
+        	//if (users!=null){
         		//si el usuario existe
-        		UserInfo usuario=users.get(0);
-        		if(usuario.getPassword().equals(greeting.getPassword())){
+        	//	UserInfo usuario=users.get(0);
+        	//	if(usuario.getPassword().equals(greeting.getPassword())){
         			//si el usuario y la contraseña coinciden
+    		
+    				UserInfo usuario=infoPort.getInfo(greeting.getLogin(),greeting.getPassword() );
         			sesion.setAttribute("login", greeting.getLogin());
         			model.addAttribute("userinfo", usuario);
         			return "result";
         			
-        		}else{
-        	        throw new HTTP404Exception();  
-        		}
-        	}
+        		//}else{
+        	       // throw new HTTP404Exception();  
+        		//}
+        	//}
         	}catch(IndexOutOfBoundsException e){
                 throw new HTTP404Exception();  
         	}
     	
-        return "result";
+       // return "result";
     }
     
     @RequestMapping(value="/cambiar-clave", method=RequestMethod.GET)
